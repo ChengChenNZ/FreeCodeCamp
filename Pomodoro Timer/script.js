fc788958c14e3bb9bed4a9a92f953e0b2239809c
cheng chen <chencheng1q88@gmail.com>
2018-01-08 22:25:30 +1300

    sccript

--- Pomodoro Timer/script.js
+++ Pomodoro Timer/script.js
@@ -82,3 +82,97 @@ function init(){
   timer = new Clock();
   anim();
 }
+
+robotoFont.load().then(
+  function(){
+    document.fonts.add(robotoFont);
+    fontLoaded = ture;
+    resetSide(1);
+    resetSide(0);
+  },
+
+  function(message){
+    console.log(message);
+  }
+);
+
+start.onclick = function(){
+  clear = false;
+  timer.start();
+}
+
+pause.onclick = function(){
+  timer.pause();
+  clear = true;
+}
+
+reset.onclick = function(){
+  clear = true;
+  work = true;
+  timer.reset();
+  resetCanvas();
+  reset.style.backgroundColor = bgColor;
+  reset.style.color = circleColor;
+}
+
+window.onresize = function(){
+  outerWidth = window.outerWidth;
+  iw = window.innerWidth;
+  h = iw > 500 ? iw * .5 : iw * .67;
+  w = iw > 500 ? iw * .75 : iw;
+  canvas.height = h;
+  canvas.width = w;
+  xr = w * 3 / 4；
+  xl = w / 4;
+  y = h / 2;
+  totRadius = w / 4;
+  minRadius = w / 9;
+  maxRadius = w / 5;
+  resetRadius();
+}
+
+document.onmouseup = function(e){
+  clicked = false;
+  delay = 300;
+}
+
+document.onmousedown = function(e){
+  clicked = true;
+  switch (e.target.id){
+    case "work-up":
+      workUp();
+      break;
+    case "work-down":
+      workDown();
+      break;
+    case "rest-up":
+      restUp();
+      break;
+    case "rest-down":
+      restDown();
+      break;
+    default:
+      break;
+  }
+}
+
+//Touch handlign
+document.addEventListener('touched',function(){
+  clicked = false;
+  delay = 300;
+});
+
+
+function workUp(){
+  if (worktime < 100 && clecked) {
+    workTime++;
+    workDisplay.innerHTML = workTime;
+    resetRadius();
+    delay *= (delay > 50) ? .85 : 1;
+    timeout = window.setTimeout(workUp,delay);
+    modified = true;
+  }else {
+    window.clearTimeout(timeout);
+    delay = 300;
+  }
+}
