53cc1012d430b06c8b6d94fce9c95367577fe1b0
cheng chen <chencheng1q88@gmail.com>
2018-01-09 20:47:29 +1300

    script

--- Pomodoro Timer/script.js
+++ Pomodoro Timer/script.js
@@ -50,7 +50,7 @@ var delay = 300;
 
 var xr = w * 3 / 4;
 var xl = w / 4;
-var y =h / 2;
+var y = h / 2;
 var totRadius = w / 4;
 var minRadius  = w / 9;
 var maxradius = w /5;
@@ -176,3 +176,240 @@ function workUp(){
     delay = 300;
   }
 }
+
+function workDown(){
+  if (workTime > 1 && clicked) {
+    workTime--;
+    workDisplay.innerHTML = workTime;
+    resetRadius();
+    delay *= (delay > 50) ? .85 : 1;
+    timeout = window.setTimeout(workDown,delay);
+    modified = true;
+  }else {
+    window.clearTimeout(timeout);
+    delay = 300;
+  }
+}
+
+function restUp(){
+  if (restTime < 100 && clicked) {
+    restTime++;
+    rest.Display.innerHTML = restTime;
+    resetRadius();
+    delay *= (delay > 50) ? .85 : 1;
+    timeout = window.setTimeout(restUp, delay);
+    modified = true;
+  }else {
+    window.clearTimeout(timeout);
+    delay = 300;
+  }
+}
+
+
+function restDown(){
+  if (restTime > 1 && clicked) {
+    restTime--;
+    restDisplay.innerHTML = restTime;
+    resetRadius();
+    delay *= (delay > 50) ? .85 : 1;
+    timeout = window.setTimeout(restDown, delay);
+    modified = 300;
+  } else {
+    window.clearTimeout(timeout);
+    delay = 300;
+  }
+}
+
+
+function correctRadius(radius){
+  var res = radius;
+  if(radius < minRadius){
+    res = minRadius;
+  }else if (radius > maxRadius){
+    res = maxRadius;
+  }
+  return res;
+}
+
+
+function resetRadius(){
+  ctx.fillstyle = bgColor;
+  ctx.fillRect(0, 0, w, h);
+  var wrkExp = math.sqrt(workTime);
+  var rstExp = math.sqrt(restTime);
+  var totExp = wrkExp + rsrExp;
+  wrkExp /= totExp;
+  rstExp /= totExp;
+  wRadius = correctRadius(wrkExp * totRadius);
+  rRadius = correctRadius(rstExp * totRadius);
+  ringWidth = wRadius < rRadius ? wRadius / 100 : rRadius / 100;
+  fRingwidth = ringWidth * 2;
+  totWidth = ringWidth + fRingwidth;
+  drawSide(1);
+  drawSide(0);
+}
+
+function fillPie(right color angle){
+  var radius = right ? wRadius : rRadius;
+  radius = correctRadius(radius);
+  var a = right ? xr : xl;
+  var b = y;
+  ctx.beginPath();
+  var thera = angle ? angle : 1;
+  ctx.moveTo(a, b);
+  ctx.lineTo(a,b + radius);
+  ctx.arc(a, b, radius, -math.PI/2, (4 * angle -1) * Math.PI / 2);
+  ctx.closePath();
+  ctx.fillstyle = color;
+  ctx.fill();
+}
+
+function drawRing(width, padding, color, right){
+  var x = right ? xr : xl;
+  var init  = right ? wRadius : rRasius;
+  init = correctRadius(init);
+  init += padding;
+  ctx.beginPath();
+  for(var i = init; i < init + width; i += .25){
+    ctx.arc(x, y, i, 0, 2*Math.PI);
+  }
+  ctx.closePath();
+  ctx.stroke();
+}
+
+function getColor(x, work){
+  var r = work ? 180 : 255;
+  var g = work ? 210 : 183;
+  var b = work ? 85 : 77;
+  r = Math.round(Math.ads(r * Math.sin(x * Math.PI)));
+  g = Math.round(Math.abs(g * Math.sin(x * Math.PI)));
+  b = Math.round(Math.abs(b * Math.sin(x * Math.PI)));
+
+  var color  = "rgb(" + r + "," + g + "," + b + ")";
+  return color;
+}
+
+function resetSide(right){
+  clearSide(right);
+  drawSide(right);
+}
+
+function drawSide(right) {
+    drawRing(ringWidth, 0, ringColor, right);
+    if (clear) {
+        displayRem(right,
+            (right ? workTime : restTime));
+    } else {
+        clearSide(!work);
+        drawRing(ringWidth, 0, ringColor, !work);
+        displayRem(!work, work ? restTime : workTime);
+    }
+}
+
+function clearSide(right) {
+    var x, r, a, b, dim;
+    var offset = 2;
+    if (right) {
+        r = wRadius + totWidth + offset;
+        x = xr - offset / 2;
+    } else {
+        r = rRadius + totWidth + offset;
+        x = xl - offset / 2;
+    }
+
+    a = x - r;
+    b = y - r;
+    dim = 2 * r;
+
+    ctx.fillStyle = bgColor;
+    ctx.fillRect(a, b, dim, dim);
+}
+
+function resetCanvas() {
+    resetRadius();
+    resetSide(1);
+    resetSide(0);
+    previous = 0;
+    firstIter = true
+}
+
+function displayRem(wrk, value) {
+    if (fontLoaded) {
+        var formatted = formatTime(value);
+        var fontSize = wrk ? 2 * wRadius / 3 : 2 * rRadius / 3;
+        if (formatted.length > 9) {
+            fontSize /= 2.5;
+        } else if (formatted.length >= 6) {
+            fontSize /= 2;
+        }
+        var xpos = wrk ? xr : xl;
+        ctx.globalCompositeOperation = "difference";
+        ctx.fillStyle = "#ffffff";
+        ctx.font = fontSize + "px " + "Roboto-Thin";
+        ctx.textAlign = "center";
+        ctx.fillText(formatted, xpos, y + fontSize / 4);
+        ctx.globalCompositeOperation = "source-over";
+    }
+}
+
+function formatTime(mins) {
+    var hours = Math.floor(mins / 60);
+    var minutes = Math.floor(mins % 60);
+    var secs = (mins - Math.floor(mins)) * 60;
+    hours = (hours > 0) ? (hours + "h") : "";
+    minutes = (minutes > 0) ? (minutes + "mn") : "";
+    secs = (secs > 0) ?
+        (parseFloat(secs).toFixed(1) + "s") : "";
+    return (hours + minutes + secs);
+}
+
+
+function anim() {
+    if (timer.running) {
+        var elapsed = timer.getElapsedTime();
+        var interval = elapsed - previous;
+        if (interval > .0001) {
+            previous = elapsed;
+            var remTime, totalTime, radius, message;
+
+            if (work) {
+                radius = wRadius;
+                totalTime = workTime;
+                message = "remaining work time = " + remTime;
+                x = xr;
+            } else {
+                radius = rRadius;
+                totalTime = restTime;
+                message = "remaining rest time = " + remTime;
+                x = xl;
+            }
+
+            //reset the inactive side of the canvas
+            if (firstIter) {
+                resetSide(!work);
+                firstIter = false;
+            }
+
+            focusColor = getColor(elapsed * 60, work);
+            remTime = totalTime - elapsed;
+
+            if (remTime > 0) {
+                resetSide(work);
+                fillPie(work, circleColor, elapsed / totalTime);
+                drawRing(fRingWidth, ringWidth, focusColor, work);
+                displayRem(work, remTime);
+            } else {
+                ding.volume = 1;
+                ding.play();
+                firstIter = true;
+                fillPie(work, circleColor, 1);
+                previous = 0;
+                work = !work;
+                timer.reset();
+                timer.start();
+            }
+        }
+    }
+}
+
+window.setInterval(anim, 10);
